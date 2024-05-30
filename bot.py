b462bc1081b43f56cc9543b0804da70939e97310
amirg <agholizadgha@mun.ca>
2024-05-29 21:48:35 -0230

    implement fastapi

--- bot.py
+++ bot.py
@@ -1,22 +1,27 @@
-from importlib.metadata import version
-
-print(version("typing_extensions"))
-
-
 from openai import OpenAI
-import openai
+from fastapi import FastAPI, Form
+from typing import Annotated
 from config import settings
-
 
 client = OpenAI(api_key = settings.openai_api_key)
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+chat_log = []
+system_message = {"role": "system",
+      "content": "You are a helpful assistant named Jarvis"}
+chat_log.append(system_message)
 
-print(completion)
+app = FastAPI()
 
+@app.post("/fitbot")
+async def fitbot(user_input: Annotated[str, Form()]):
+
+  chat_log.append({"role": "user", "content": user_input})
+  response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=chat_log,
+    temperature=0.6
+  )
+
+  bot_response = response.choices[0].message.content
+  chat_log.append({"role": "assistant", "content": bot_response})
+  return bot_response
