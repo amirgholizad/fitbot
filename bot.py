c3eddc22baf86675bc3b07520a34b9adafcc6292
amirg <agholizadgha@mun.ca>
2024-05-31 19:09:28 -0230

    trying to make streming work

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 from openai import OpenAI
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Form, Request, WebSocket
 from typing import Annotated
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from config import settings
 
@@ -14,12 +15,41 @@ system_message = {"role": "system",
 chat_log.append(system_message)
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def chat_page(request: Request):
   return templates.TemplateResponse("bot.html", {"request": request})
+
+
+@app.websocket("/ws")
+async def chat(websocket: WebSocket):
+  await websocket.accept()
+  while True:
+    user_input = await websocket.receive_text()
+    chat_log.append({"role": "user", "content": user_input})
+    chat_responses.append(user_input)
+
+    try:
+      response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=chat_log,
+        temperature=0.6,
+        stream=True
+      )
+      ai_response = ''
+
+      for chunk in response:
+        if chunk.choices[0].delta.content is not None:
+          ai_response += chunk.choices[0].delta.content
+          await websocket.send_text(chunk.choices[0].delta.content)
+      chat_responses.append(ai_response)
+      
+    except Exception as e:
+      await websocket.send_text(f"Error: {e}")
+      break
 
 @app.post("/", response_class=HTMLResponse)
 async def chat(request: Request, user_input: Annotated[str, Form()]):
