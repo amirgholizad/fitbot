37de6a3b07708a45d6ea7ac93b5ed7a2604213a9
amirg <agholizadgha@mun.ca>
2024-06-01 22:10:42 -0230

    ready to deploy

--- static/script.js
+++ static/script.js
@@ -1,43 +1,25 @@
-var ws = new WebSocket("ws://localhost:8000/ws");
+var WebSocketString = "";
+
+if (window.location.hostname == "127.0.0.1") {
+  WebSocketString = "ws://localhost:8000/ws";
+} else {
+  WebSocketString = "wss://${window.location.hostname}$/ws";
+}
+
+var ws = new WebSocket(WebSocketString);
 var sendButton = document.getElementById("sendButton");
 var userInput = document.getElementById("userInput");
 var chatHistory = document.getElementById("chatHistory");
-var lastUserMessageDiv = null;
-var isNewUserInput = true;
 
 ws.onmessage = function(event) {
-  var message = event.data.trim();
-
-  if (lastUserMessageDiv && !isNewUserInput) {
-    var shouldAddSpace = true;
-    var noPrependSpaceChars = [",", ".", "!", "?", ";", ":", "'"];
-
-    if (noPrependSpaceChars.includes(message.charAt(0))) {
-      shouldAddSpace = false;
-    }
-
-    lastUserMessageDiv.textContent += (shouldAddSpace ? " " : "") + message;
-  } else {
-    var messageDiv = document.createElement("div");
-    messageDiv.className = "chat-message ai-response";
-    messageDiv.textContent = message;
-    chatHistory.appendChild(messageDiv);
-    lastUserMessageDiv = messageDiv;
-    isNewUserInput = false;
-  }
+  var message = event.data;
+  var messageDiv = document.createElement("div");
+  messageDiv.textContent = "- " + message;
+  chatHistory.appendChild(messageDiv);
 };
 
 sendButton.onclick = function() {
-  var message = userInput.value.trim();
-  if (message) {
-    var userInputDiv = document.createElement("div");
-    userInputDiv.className = "chat-message user-input";
-    userInputDiv.textContent = message;
-    chatHistory.appendChild(userInputDiv);
-    chatHistory.scrollTop = chatHistory.scrollHeight;
-    ws.send(message);
-    userInput.value = "";
-    isNewUserInput = true;
-    lastUserMessageDiv = null;
-  }
+  var message = userInput.value;
+  ws.send(message);
+  userInput.value = "";
 };
